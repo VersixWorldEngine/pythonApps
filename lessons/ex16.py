69d92dc018c083a87bbde46528c717ec36be1111
WorldEngine <engine.world@versix.org>
2015-03-13 21:18:14 -0700

    fixed raw_input

--- lessons/ex16.py
+++ lessons/ex16.py
@@ -6,7 +6,7 @@ print "We're going to erase %r." % filename
 print "If you don't want that, hit CTRL-C (^C)."
 print  "If you do want that, hit RETURN."
 
-raw_input(?)
+raw_input("?")
 
 print "Opening the file..."
 target = open(filename, 'w')
